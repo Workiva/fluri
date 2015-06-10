d110dde67b430be95f0f64c6430e5b583d1a317c
evanweible-wf <evan.weible@webfilings.com>
2015-06-10 13:22:14 -0500

    Coverage

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Clean out old coverage artifacts
 if [ -d "./coverage_report" ]; then
     rm -rf ./coverage_report
 fi
@@ -7,5 +8,11 @@ if [ -f "./coverage.lcov" ]; then
     rm ./coverage.lcov
 fi
 
+# Collect coverage and generate report
 pub get
-pub global run dart_codecov_generator:generate_coverage --report-on=lib/ "$@"
+pub run dart_codecov_generator --report-on=lib/ "$@"
+
+# Open HTML report if successful
+if [ $? -eq 0 ] && [ -f "./coverage_report/index.html" ]; then
+    open coverage_report/index.html
+fi
