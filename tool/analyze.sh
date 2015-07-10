3921a4d59ebb1e8909c5deebec16f890ad41ed2a
Trent Grover <trent.grover@webfilings.com>
2015-07-10 02:04:04 -0500

    add analyze script for CI

--- tool/analyze.sh
+++ tool/analyze.sh
@@ -1,3 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-dartanalyzer --fatal-warnings --no-hints lib/*.dart test/*.dart
+EXIT_STATUS=0
+
+# Find all sub-directories of the current directory with a pubspec.yaml and run dartanalyzer on it
+
+directories=(`find . -name pubspec.yaml -print0 | xargs -0 -n1 dirname | sort --unique`)
+rootDirectory=$(pwd)
+
+for pubDirectory in ${directories[*]}
+do
+    if [ -d "$pubDirectory" ]
+    then
+        echo Analyze "'${pubDirectory}'"...
+        cd "$pubDirectory"
+        ANALYZE_DIRECTORIES=""
+        subdirectories=( lib web test )
+        for dir in "${subdirectories[@]}"
+        do
+        	if [ -d $dir ]
+        	then
+        		ANALYZE_DIRECTORIES="$ANALYZE_DIRECTORIES $dir/*.dart"
+			fi
+        done
+		if [ -z "$ANALYZE_DIRECTORIES" ]
+		then
+			echo "nothing to analyze"
+		else
+			echo "dartanalyzer --fatal-warnings --no-hints $ANALYZE_DIRECTORIES"
+			dartanalyzer --fatal-warnings --no-hints $ANALYZE_DIRECTORIES || EXIT_STATUS=$?
+        fi
+        echo "Current EXIT_STATUS = $EXIT_STATUS"
+        cd "$rootDirectory"
+        echo
+    fi
+done
+
+exit $EXIT_STATUS
